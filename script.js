6f1ee8023486d2fa27d6dab8337549a7336ac9dc
pelm3shka1 <edovsky@htmail.com>
2026-01-22 20:45:34 +0200

    form and style

---
+++ script.js
@@ -0,0 +1,25 @@
+const carForm = document.getElementById("carForm");
+carsDiv = document.getElementById("cars");
+
+carForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const description = document.getElementById("description").value;
+  const image = document.getElementById("image").value;
+  const fuel = document.getElementById("fuel").value;
+  const price = document.getElementById("price").value;
+
+  const carCard = document.createElement("div");
+  carCard.className = "car-card";
+
+  carCard.innerHTML = `
+  <img src=${image} alt="car">
+  <h3>${name}</h3>
+  <p>${description}</p>
+  <p>Auto kaina: ${price}</p>
+  <p>Auto kuras: ${fuel}</p>
+  `;
+
+  carsDiv.appendChild(carCard);
+});
